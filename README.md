e35d96ced86e14286afb56cf4f6b4d4238649d7f
Catchears <catchears@gmail.com>
2023-06-28 18:43:35 +0200

    inital version

--- README.md
+++ README.md
@@ -1,95 +1,23 @@
-# Obsidian Sample Plugin
+# Obsidian Hidden Comments
 
-This is a sample plugin for Obsidian (https://obsidian.md).
+Simple plugin to create hidden comments in your [obsidian.md](https://obsidian.md) vault.
 
-This project uses Typescript to provide type checking and documentation.
-The repo depends on the latest plugin API (obsidian.d.ts) in Typescript Definition format, which contains TSDoc comments describing what it does.
+## Fair Disclaimer
 
-**Note:** The Obsidian API is still in early alpha and is subject to change at any time!
+This is my first time working with TypeScript (or JavaScript) - this won't be perfect.
 
-This sample plugin demonstrates some of the basic functionality the plugin API can do.
-- Adds a ribbon icon, which shows a Notice when clicked.
-- Adds a command "Open Sample Modal" which opens a Modal.
-- Adds a plugin setting tab to the settings page.
-- Registers a global click event and output 'click' to the console.
-- Registers a global interval which logs 'setInterval' to the console.
+The plugin cannot delete files, but will rename the folder with the name given in the plugin settings to show / hide the files.
 
-## First time developing plugins?
+## Installation
 
-Quick starting guide for new plugin devs:
+Simply clone the repository to your plugins directory.
 
-- Check if [someone already developed a plugin for what you want](https://obsidian.md/plugins)! There might be an existing plugin similar enough that you can partner up with.
-- Make a copy of this repo as a template with the "Use this template" button (login to GitHub if you don't see it).
-- Clone your repo to a local development folder. For convenience, you can place this folder in your `.obsidian/plugins/your-plugin-name` folder.
-- Install NodeJS, then run `npm i` in the command line under your repo folder.
-- Run `npm run dev` to compile your plugin from `main.ts` to `main.js`.
-- Make changes to `main.ts` (or create new `.ts` files). Those changes should be automatically compiled into `main.js`.
-- Reload Obsidian to load the new version of your plugin.
-- Enable plugin in settings window.
-- For updates to the Obsidian API run `npm update` in the command line under your repo folder.
+## Usage
 
-## Releasing new releases
+To create a new hidden comment, either use the `Create New Hidden Comment` command or select the text you'd like hidden and use the `Hide Selection in Comment` command.
 
-- Update your `manifest.json` with your new version number, such as `1.0.1`, and the minimum Obsidian version required for your latest release.
-- Update your `versions.json` file with `"new-plugin-version": "minimum-obsidian-version"` so older versions of Obsidian can download an older version of your plugin that's compatible.
-- Create new GitHub release using your new version number as the "Tag version". Use the exact version number, don't include a prefix `v`. See here for an example: https://github.com/obsidianmd/obsidian-sample-plugin/releases
-- Upload the files `manifest.json`, `main.js`, `styles.css` as binary attachments. Note: The manifest.json file must be in two places, first the root path of your repository and also in the release.
-- Publish the release.
+To show or hide your comments, use the `Hide Comments` or `Show Comments` command respectively. Alternatively, you can use the toggle in the plugin settings.
 
-> You can simplify the version bump process by running `npm version patch`, `npm version minor` or `npm version major` after updating `minAppVersion` manually in `manifest.json`.
-> The command will bump version in `manifest.json` and `package.json`, and add the entry for the new version to `versions.json`
+You can also bind keys to these commands if you wish.
 
-## Adding your plugin to the community plugin list
-
-- Check https://github.com/obsidianmd/obsidian-releases/blob/master/plugin-review.md
-- Publish an initial version.
-- Make sure you have a `README.md` file in the root of your repo.
-- Make a pull request at https://github.com/obsidianmd/obsidian-releases to add your plugin.
-
-## How to use
-
-- Clone this repo.
-- `npm i` or `yarn` to install dependencies
-- `npm run dev` to start compilation in watch mode.
-
-## Manually installing the plugin
-
-- Copy over `main.js`, `styles.css`, `manifest.json` to your vault `VaultFolder/.obsidian/plugins/your-plugin-id/`.
-
-## Improve code quality with eslint (optional)
-- [ESLint](https://eslint.org/) is a tool that analyzes your code to quickly find problems. You can run ESLint against your plugin to find common bugs and ways to improve your code. 
-- To use eslint with this project, make sure to install eslint from terminal:
-  - `npm install -g eslint`
-- To use eslint to analyze this project use this command:
-  - `eslint main.ts`
-  - eslint will then create a report with suggestions for code improvement by file and line number.
-- If your source code is in a folder, such as `src`, you can use eslint with this command to analyze all files in that folder:
-  - `eslint .\src\`
-
-## Funding URL
-
-You can include funding URLs where people who use your plugin can financially support it.
-
-The simple way is to set the `fundingUrl` field to your link in your `manifest.json` file:
-
-```json
-{
-    "fundingUrl": "https://buymeacoffee.com"
-}
-```
-
-If you have multiple URLs, you can also do:
-
-```json
-{
-    "fundingUrl": {
-        "Buy Me a Coffee": "https://buymeacoffee.com",
-        "GitHub Sponsor": "https://github.com/sponsors",
-        "Patreon": "https://www.patreon.com/"
-    }
-}
-```
-
-## API Documentation
-
-See https://github.com/obsidianmd/obsidian-api
+By default, the plugin will attempt to apply the `hide-embed-title` CSS snippet to all notes in which you use a command to create a new hidden comment to remove the embedded note name and first-level headings. This will also mostly hide the error embed when the files are hidden. **This can be disabled in the plugin settings.**
--- main.ts
+++ main.ts
@@ -1,137 +1,453 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {
+	App,
+	Editor,
+	MarkdownView,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+	TFolder,
+	normalizePath,
+} from "obsidian";
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+interface HiddenCommentsPluginSettings {
+	showComments: boolean;
+	hiddenFolderName: string;
+	commentFilePrefix: string;
+	setCssClass: boolean;
+	hideEmbedTitles: boolean;
+	showOnQuit: boolean;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+const DEFAULT_SETTINGS: HiddenCommentsPluginSettings = {
+	showComments: true,
+	hiddenFolderName: "hiddenComments",
+	commentFilePrefix: "comment-",
+	setCssClass: true,
+	hideEmbedTitles: true,
+	showOnQuit: false,
+};
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class HiddenCommentsPlugin extends Plugin {
+	settings: HiddenCommentsPluginSettings;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
 		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
+			id: "show-hidden-comments",
+			name: "Show comments",
 			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+				if (checking) {
+					return !this.settings.showComments;
+				} else {
+					this.showComments(false);
 				}
+			},
+		});
+
+		this.addCommand({
+			id: "hide-hidden-comments",
+			name: "Hide comments",
+			checkCallback: (checking: boolean) => {
+				if (checking) {
+					return this.settings.showComments;
+				} else {
+					this.hideComments(false);
+				}
+			},
+		});
+
+		this.addCommand({
+			id: "unload-self",
+			name: "Unload Self",
+			callback: () => {
+				this.unload();
+			},
+		});
+
+		this.addCommand({
+			id: "hide-selection-in-comment",
+			name: "Hide Selection in Comment",
+			editorCheckCallback: (
+				checking: boolean,
+				editor: Editor,
+				view: MarkdownView
+			) => {
+				if (checking) {
+					return this.settings.showComments;
+				} else {
+					const selection = editor.getSelection();
+
+					const contents =
+						this.generateHiddenPreamble(view.file.name) + selection;
+
+					const commentFileName = this.createHiddenFile(
+						view,
+						contents
+					);
+
+					editor.replaceSelection(
+						"![[" + commentFileName + "#Comments]]"
+					);
+
+					if (this.settings.setCssClass) {
+						async () => {
+							try {
+								await this.addFrontmatterCssClass(view, editor);
+							} catch (error) {
+								new Notice("Couldn't set cssclass!");
+							}
+						};
+					}
+				}
+			},
+		});
+
+		this.addCommand({
+			id: "create-hidden-comment",
+			name: "Create New Hidden Comment",
+			editorCheckCallback: async (
+				checking: boolean,
+				editor: Editor,
+				view: MarkdownView
+			) => {
+				if (checking) {
+					return this.settings.showComments;
+				} else {
+					const contents = this.generateHiddenPreamble(
+						view.file.name
+					);
+
+					const commentFileName = this.createHiddenFile(
+						view,
+						contents
+					);
+
+					const oldCursor = editor.getCursor();
+					const newCursor = editor.getCursor();
+
+					editor.replaceRange(
+						"![[" + commentFileName + "#Comments]]",
+						editor.getCursor()
+					);
+
+					newCursor.ch = oldCursor.ch + commentFileName.length + 14;
+					editor.setCursor(newCursor);
+
+					if (this.settings.setCssClass) {
+						await this.addFrontmatterCssClass(view, editor);
+					}
+				}
+			},
+		});
+
+		this.addSettingTab(new HiddenCommentsSettingsTab(this.app, this));
+
+		const hiddenFolderExists = await this.folderExists(
+			"." + this.settings.hiddenFolderName
+		);
+		const visibleFolderExists = await this.folderExists(
+			this.settings.hiddenFolderName
+		);
+
+		if (
+			!this.settings.showComments &&
+			visibleFolderExists &&
+			!hiddenFolderExists
+		) {
+			// If the folder was made visible on close last exit.
+			await this.renameToHide(true);
+		} else if (!hiddenFolderExists && visibleFolderExists) {
+			this.settings.showComments = true;
+			await this.saveSettings();
+		} else if (hiddenFolderExists && !visibleFolderExists) {
+			this.settings.showComments = false;
+			await this.saveSettings();
+		} else if (hiddenFolderExists && visibleFolderExists) {
+			new Notice(
+				"Both hidden and visible folders exist! Please delete one."
+			);
+			this.unload();
+		} else {
+			const hiddenFolder = await this.app.vault.getAbstractFileByPath(
+				(await this.prefix()) + this.settings.hiddenFolderName
+			);
+
+			if (!(hiddenFolder instanceof TFolder)) {
+				await this.app.vault.createFolder(
+					(await this.prefix()) + this.settings.hiddenFolderName
+				);
 			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		}
 	}
 
-	onunload() {
+	async addFrontmatterCssClass(view: MarkdownView, editor: Editor) {
+		const currentFileContents = await this.app.vault.read(view.file);
+		if (currentFileContents.includes("cssclass: hide-embed-title\n")) {
+			// pass
+		} else {
+			if (currentFileContents.startsWith("---")) {
+				if (currentFileContents.includes("\ncssclass: ")) {
+					new Notice("Couldn't set cssclass!");
+					return;
+				} else {
+					const pos = this.customEditorPosition(editor, 1, 0);
+					editor.replaceRange(
+						"cssclass: hide-embed-title\n",
+						pos,
+						pos
+					);
+				}
+			} else {
+				const startOfFile = editor.getCursor();
+				startOfFile.ch = 0;
+				startOfFile.line = 0;
+				editor.replaceRange(
+					"---\ncssclass: hide-embed-title\n---\n",
+					startOfFile
+				);
+			}
+		}
+	}
 
+	generateHiddenPreamble(fileName: string) {
+		return (
+			"---\ncssclass: hide-embed-title\n---\nOriginal File: [[" +
+			fileName +
+			"]]\n# Comments\n"
+		);
+	}
+
+	createHiddenFile(view: MarkdownView, contents: string): string {
+		const files = this.app.vault.getMarkdownFiles().map((x) => x.name);
+		for (let incr = 1; incr < 1000; incr++) {
+			const commentFileName =
+				this.settings.commentFilePrefix +
+				incr.toString() +
+				"-" +
+				view.file.name;
+			if (!files.includes(commentFileName)) {
+				break;
+			}
+		}
+		const commentFileName =
+			this.settings.commentFilePrefix +
+			incr.toString() +
+			"-" +
+			view.file.name;
+		const fullPath = normalizePath(
+			this.settings.hiddenFolderName + "/" + commentFileName
+		);
+		this.app.vault.create(fullPath, contents);
+		return commentFileName;
+	}
+
+	async onunload() {
+		// @ts-expect-error: private the attribute exists, just isn't documented
+		await app.plugins.unloadPlugin(this.manifest.id);
+		if (this.settings.showOnQuit) {
+			const hiddenFolderExists = await this.folderExists(
+				"." + this.settings.hiddenFolderName
+			);
+			const visibleFolderExists = await this.folderExists(
+				this.settings.hiddenFolderName
+			);
+			if (!(hiddenFolderExists && visibleFolderExists)) {
+				this.renameToShow(true);
+			}
+		}
+		console.log("Unloading " + this.manifest.id);
+	}
+
+	async folderExists(name: string) {
+		if (!name.startsWith(".")) {
+			// This will *not* find folders with names that begin with a period (.).
+			const folder = this.app.vault.getAbstractFileByPath(name);
+			return folder instanceof TFolder;
+		} else {
+			// This will. Using `.adapter` isn't recommended, but appears needed in this case.
+			return await this.app.vault.adapter.exists(name);
+		}
+	}
+
+	async fileExists(name: string) {
+		const file = this.app.vault.getAbstractFileByPath(name);
+		return file instanceof TFile;
+	}
+
+	async prefix() {
+		return this.settings.showComments ? new String() : ".";
 	}
 
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
+	async showComments(silent: boolean) {
+		if (this.settings.showComments) {
+			if (!silent) {
+				new Notice("Comments should already be visible!");
+			}
+		} else {
+			this.settings.showComments = true;
+			await this.saveSettings();
+
+			await this.renameToShow(silent);
+		}
+	}
+
+	async hideComments(silent: boolean) {
+		if (!this.settings.showComments) {
+			if (!silent) {
+				new Notice("Comments should already be hidden!");
+			}
+		} else {
+			this.settings.showComments = false;
+			await this.saveSettings();
+
+			await this.renameToHide(silent);
+		}
+	}
+
+	async renameToShow(silent: boolean) {
+		const oldFolderName: string = "." + this.settings.hiddenFolderName;
+		const newFolderName: string = this.settings.hiddenFolderName;
+		await this.renameFolder(oldFolderName, newFolderName, silent);
+	}
+
+	async renameToHide(silent: boolean) {
+		const oldFolderName: string = this.settings.hiddenFolderName;
+		const newFolderName: string = "." + this.settings.hiddenFolderName;
+		await this.renameFolder(oldFolderName, newFolderName, silent);
+	}
+
+	async renameFolder(oldName: string, newName: string, silent: boolean) {
+		const folder = await this.app.vault.getAbstractFileByPath(oldName);
+		const really_exists = await this.folderExists(oldName);
+
+		if (folder instanceof TFolder) {
+			await this.app.vault.rename(folder, newName);
+		} else if (really_exists) {
+			// `.adapter` needed to be able to see hidden folder
+			await this.app.vault.adapter.rename(oldName, newName);
+		} else {
+			if (!silent) {
+				new Notice("Comments folder couldn't be found!");
+			}
+		}
+	}
+
+	customEditorPosition(editor: Editor, line: number, ch: number) {
+		const pos = editor.getCursor();
+		pos.line = line;
+		pos.ch = ch;
+		return pos;
+	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class HiddenCommentsSettingsTab extends PluginSettingTab {
+	plugin: HiddenCommentsPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: HiddenCommentsPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl("h2", { text: "Hidden Comments Settings" });
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setName("Show Comments")
+			.setDesc("Current visibility of hidden comments")
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.showComments)
+					.onChange(async (value: boolean) => {
+						if (value === true) {
+							await this.plugin.showComments(false);
+						} else {
+							await this.plugin.hideComments(false);
+						}
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Comment Folder Name")
+			.setDesc("Name of the folder comments are stored in")
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.hiddenFolderName)
+					.setPlaceholder(DEFAULT_SETTINGS.hiddenFolderName)
+					.onChange(async (value: string) => {
+						const prefix = await this.plugin.prefix();
+						const oldFolderName: string =
+							prefix + this.plugin.settings.hiddenFolderName;
+						const newFolderName: string = prefix + value;
+						await this.plugin.renameFolder(
+							oldFolderName,
+							newFolderName,
+							false
+						);
+						this.plugin.settings.hiddenFolderName = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Default Comment File Prefix")
+			.setDesc(
+				"The default prefix added to the file names of comment files; won't change already created files."
+			)
+			.addText((text) =>
+				text
+					.setValue(this.plugin.settings.commentFilePrefix)
+					.setPlaceholder(DEFAULT_SETTINGS.commentFilePrefix)
+					.onChange(async (value: string) => {
+						this.plugin.settings.commentFilePrefix = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Set CSS Class / Hide Embed Titles")
+			.setDesc(
+				"Sets the `cssclass` property of files when a hidden comment is created. This will hide the file name and any level 1 headings."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.setCssClass)
+					.onChange(async (value: boolean) => {
+						this.plugin.settings.setCssClass = value;
+						await this.plugin.saveSettings();
+					})
+			);
+
+		new Setting(containerEl)
+			.setName("Show Comments on Quit")
+			.setDesc(
+				"Make comments visible while Obsidian is closed. Will re-hide once obsidian starts if they were hidden before."
+			)
+			.addToggle((toggle) =>
+				toggle
+					.setValue(this.plugin.settings.showOnQuit)
+					.onChange(async (value: boolean) => {
+						this.plugin.settings.showOnQuit = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
